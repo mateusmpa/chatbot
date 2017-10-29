1c380ca4aa3189ade304aa97522a41cc9d8f08ba
Mateus Alves <mateusmpa@gmail.com>
2017-10-29 12:30:23 -0200

    Fix bug HasManyThroughAssociationNotFoundError

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -1,7 +1,7 @@
 class Hashtag < ActiveRecord::Base
   validates_presence_of :name
 
-  has_many :faqs_hashtag
-  has_many :faqs, through: :faqs_hashtags
+  has_many :faq_hashtags
+  has_many :faqs, through: :faq_hashtags
   belongs_to :company
 end
